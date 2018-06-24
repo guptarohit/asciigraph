82109e6e721ed5379054f1e18eef4602a4f48a39
Rohit Gupta <rohitg.tech@gmail.com>
2018-06-24 18:24:13 +0530

    Fix graph height issue. - the height of graph with interval(max-min) < 0 was evaluating to zero.

--- asciigraph.go
+++ asciigraph.go
@@ -20,16 +20,20 @@ func Plot(series []float64, config map[string]interface{}) string {
 
 	interval := math.Abs(maximum - minimum)
 
+	if val, ok := config["height"].(int); ok {
+		height = val
+	} else {
+		if int(interval) <= 0 {
+			height = int(interval * math.Pow10(int(math.Ceil(-math.Log10(interval)))))
+		} else {
+			height = int(interval)
+		}
+	}
+
 	if val, ok := config["offset"].(int); ok {
 		offset = val
 	} else {
 		offset = 3
-	}
-
-	if val, ok := config["height"].(int); ok {
-		height = val
-	} else {
-		height = int(interval)
 	}
 
 	if val, ok := config["caption"].(string); ok {
