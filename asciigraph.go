c819fc4077eb2b02bed4075e78beb161bbefcd53
bnMikheili <mnazg16@freeuni.edu.ge>
2020-06-08 17:31:07 +0400

    divide into contructor/plot

--- asciigraph.go
+++ asciigraph.go
@@ -10,34 +10,41 @@ import (
 type Graph struct {
 	series []float64
 	config *config
+
+	maxWidth  int
+	intmin2   int
+	intmax2   int
+	rows      int
+	plot      [][]string
+	maximum   float64
+	minimum   float64
+	ratio     float64
+	interval  float64
+	min2      float64
+	precision int
 }
 
 func NewGraph(series []float64, options ...Option) *Graph {
 	g := new(Graph)
+
 	g.series = series
 	g.config = configure(config{
 		Offset: 3,
 	}, options)
 
-	return g
-}
-
-// Plot returns ascii graph for a series.
-func (g Graph) Plot() string {
 	var logMaximum float64
 
 	if g.config.Width > 0 {
 		g.series = interpolateArray(g.series, g.config.Width)
 	}
-
-	minimum, maximum := minMaxFloat64Slice(g.series)
-	interval := math.Abs(maximum - minimum)
+	g.minimum, g.maximum = minMaxFloat64Slice(g.series)
+	g.interval = math.Abs(g.maximum - g.minimum)
 
 	if g.config.Height <= 0 {
-		if int(interval) <= 0 {
-			g.config.Height = int(interval * math.Pow10(int(math.Ceil(-math.Log10(interval)))))
+		if int(g.interval) <= 0 {
+			g.config.Height = int(g.interval * math.Pow10(int(math.Ceil(-math.Log10(g.interval)))))
 		} else {
-			g.config.Height = int(interval)
+			g.config.Height = int(g.interval)
 		}
 	}
 
@@ -45,35 +52,32 @@ func (g Graph) Plot() string {
 		g.config.Offset = 3
 	}
 
-	var ratio float64
-	if interval != 0 {
-		ratio = float64(g.config.Height) / interval
+	if g.interval != 0 {
+		g.ratio = float64(g.config.Height) / g.interval
 	} else {
-		ratio = 1
+		g.ratio = 1
 	}
-	min2 := round(minimum * ratio)
-	max2 := round(maximum * ratio)
+	g.min2 = round(g.minimum * g.ratio)
+	max2 := round(g.maximum * g.ratio)
 
-	intmin2 := int(min2)
-	intmax2 := int(max2)
+	g.intmin2 = int(g.min2)
+	g.intmax2 = int(max2)
 
-	rows := int(math.Abs(float64(intmax2 - intmin2)))
+	g.rows = int(math.Abs(float64(g.intmax2 - g.intmin2)))
 	width := len(g.series) + g.config.Offset
 
-	var plot [][]string
-
 	// initialise empty 2D grid
-	for i := 0; i < rows+1; i++ {
+	for i := 0; i < g.rows+1; i++ {
 		var line []string
 		for j := 0; j < width; j++ {
 			line = append(line, " ")
 		}
-		plot = append(plot, line)
+		g.plot = append(g.plot, line)
 	}
 
-	precision := 2
-	logMaximum = math.Log10(math.Max(math.Abs(maximum), math.Abs(minimum))) //to find number of zeros after decimal
-	if minimum == float64(0) && maximum == float64(0) {
+	g.precision = 2
+	logMaximum = math.Log10(math.Max(math.Abs(g.maximum), math.Abs(g.minimum))) //to find number of zeros after decimal
+	if g.minimum == float64(0) && g.maximum == float64(0) {
 		logMaximum = float64(-1)
 	}
 
@@ -81,69 +85,74 @@ func (g Graph) Plot() string {
 		// negative log
 		if math.Mod(logMaximum, 1) != 0 {
 			// non-zero digits after decimal
-			precision = precision + int(math.Abs(logMaximum))
+			g.precision = g.precision + int(math.Abs(logMaximum))
 		} else {
-			precision = precision + int(math.Abs(logMaximum)-1.0)
+			g.precision = g.precision + int(math.Abs(logMaximum)-1.0)
 		}
 	} else if logMaximum > 2 {
-		precision = 0
+		g.precision = 0
 	}
 
-	maxNumLength := len(fmt.Sprintf("%0.*f", precision, maximum))
-	minNumLength := len(fmt.Sprintf("%0.*f", precision, minimum))
-	maxWidth := int(math.Max(float64(maxNumLength), float64(minNumLength)))
+	maxNumLength := int(len(fmt.Sprintf("%0.*f", g.precision, g.maximum)))
+	minNumLength := int(len(fmt.Sprintf("%0.*f", g.precision, g.minimum)))
+	g.maxWidth = int(math.Max(float64(maxNumLength), float64(minNumLength)))
+
+	return g
+}
+
+func (g *Graph) Plot() string {
 
 	// axis and labels
-	for y := intmin2; y < intmax2+1; y++ {
+	for y := g.intmin2; y < g.intmax2+1; y++ {
 		var magnitude float64
-		if rows > 0 {
-			magnitude = maximum - (float64(y-intmin2) * interval / float64(rows))
+		if g.rows > 0 {
+			magnitude = g.maximum - (float64(y-g.intmin2) * g.interval / float64(g.rows))
 		} else {
 			magnitude = float64(y)
 		}
 
-		label := fmt.Sprintf("%*.*f", maxWidth+1, precision, magnitude)
-		w := y - intmin2
+		label := fmt.Sprintf("%*.*f", g.maxWidth+1, g.precision, magnitude)
+		w := y - g.intmin2
 		h := int(math.Max(float64(g.config.Offset)-float64(len(label)), 0))
 
-		plot[w][h] = label
+		g.plot[w][h] = label
 		if y == 0 {
-			plot[w][g.config.Offset-1] = "┼"
+			g.plot[w][g.config.Offset-1] = "┼"
 		} else {
-			plot[w][g.config.Offset-1] = "┤"
+			g.plot[w][g.config.Offset-1] = "┤"
 		}
 	}
 
-	y0 := int(round(g.series[0]*ratio) - min2)
+	y0 := int(round(g.series[0]*g.ratio) - g.min2)
 	var y1 int
 
-	plot[rows-y0][g.config.Offset-1] = "┼" // first value
+	g.plot[g.rows-y0][g.config.Offset-1] = "┼" // first value
 
-	for x := 0; x < len(g.series)-1; x++ { // plot the line
-		y0 = int(round(g.series[x+0]*ratio) - float64(intmin2))
-		y1 = int(round(g.series[x+1]*ratio) - float64(intmin2))
+	for x := 0; x < len(g.series)-1; x++ { // g.plot the line
+		y0 = int(round(g.series[x+0]*g.ratio) - float64(g.intmin2))
+		y1 = int(round(g.series[x+1]*g.ratio) - float64(g.intmin2))
 		if y0 == y1 {
-			plot[rows-y0][x+g.config.Offset] = "─"
+			g.plot[g.rows-y0][x+g.config.Offset] = "─"
 		} else {
 			if y0 > y1 {
-				plot[rows-y1][x+g.config.Offset] = "╰"
-				plot[rows-y0][x+g.config.Offset] = "╮"
+				g.plot[g.rows-y1][x+g.config.Offset] = "╰"
+				g.plot[g.rows-y0][x+g.config.Offset] = "╮"
 			} else {
-				plot[rows-y1][x+g.config.Offset] = "╭"
-				plot[rows-y0][x+g.config.Offset] = "╯"
+				g.plot[g.rows-y1][x+g.config.Offset] = "╭"
+				g.plot[g.rows-y0][x+g.config.Offset] = "╯"
 			}
 
 			start := int(math.Min(float64(y0), float64(y1))) + 1
 			end := int(math.Max(float64(y0), float64(y1)))
 			for y := start; y < end; y++ {
-				plot[rows-y][x+g.config.Offset] = "│"
+				g.plot[g.rows-y][x+g.config.Offset] = "│"
 			}
 		}
 	}
 
 	// join columns
 	var lines bytes.Buffer
-	for h, horizontal := range plot {
+	for h, horizontal := range g.plot {
 		if h != 0 {
 			lines.WriteRune('\n')
 		}
@@ -155,9 +164,12 @@ func (g Graph) Plot() string {
 	// add caption if not empty
 	if g.config.Caption != "" {
 		lines.WriteRune('\n')
-		lines.WriteString(strings.Repeat(" ", g.config.Offset+maxWidth+2))
+		lines.WriteString(strings.Repeat(" ", g.config.Offset+g.maxWidth+2))
 		lines.WriteString(g.config.Caption)
 	}
+	fmt.Printf("\n")
+	fmt.Printf(lines.String())
+	fmt.Printf("\n")
 	return lines.String()
 }
 
