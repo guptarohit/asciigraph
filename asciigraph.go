89edd584554c20f85373fe2100262f7e0146bab5
Nathan Baulch <nathan.baulch@gmail.com>
2022-01-22 22:22:22 +1100

    Fix incorrect y-axis start value tick

--- asciigraph.go
+++ asciigraph.go
@@ -96,11 +96,7 @@ func Plot(series []float64, options ...Option) string {
 		h := int(math.Max(float64(config.Offset)-float64(len(label)), 0))
 
 		plot[w][h] = label
-		if y == 0 {
-			plot[w][config.Offset-1] = "┼"
-		} else {
-			plot[w][config.Offset-1] = "┤"
-		}
+		plot[w][config.Offset-1] = "┤"
 	}
 
 	y0 := int(round(series[0]*ratio) - min2)
